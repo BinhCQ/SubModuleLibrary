faac15499e5f40b2efe4da95cfc9bba3a0b47bbb
Justin CAO <CaoQuangBinh@Gmail.com>
2015-08-23 21:52:26 +0700

    [Add] bar()-method

--- submodulelibrary/src/main/java/com/appconus/submodulelibrary/Foo.java
+++ submodulelibrary/src/main/java/com/appconus/submodulelibrary/Foo.java
@@ -4,4 +4,7 @@ public class Foo {
     public static String ping(){
         return "Reply from Foo class in sub-module library";
     }
+    public static String bar(){
+        return "bar";
+    }
 }
